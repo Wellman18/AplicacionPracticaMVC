f80e6f2929c399b46ddc423fafb0943ab94935ff
Wellman Aranda <wellmanaranda@rocketmail.com>
2025-09-23 20:14:31 -0600

    Se modifica la clase program.cs

---
+++ FinanzaPersonalApp/FinanzaPersonalApp/Models/ConnectionManagerDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FinanzaPersonalApp.Models
+{
+    public class ConnectionManagerDbContext : DbContext
+    {
+        public ConnectionManagerDbContext(DbContextOptions<ConnectionManagerDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+        public DbSet<Models.Usuario> Usuarios { get; set; }
+    }
+}
